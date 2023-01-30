d320657d4426260c14d75d83cd43931278bac5b0
Anthony Preucil <60394347+apreucil@users.noreply.github.com>
2023-01-30 11:41:47 -0500

    Update WeatherCube.py
    test stevens point, wi

--- WeatherCube.py
+++ WeatherCube.py
@@ -53,7 +53,7 @@ pi = pigpio.pi()
 
 #%% Get curernt Location of Device and Locations of Stations
 
-zipcode = "79760"
+zipcode = "54481"
 test_loc = True
 myloc = geocoder.ip('me')
 if myloc.latlng == None or test_loc==True:
