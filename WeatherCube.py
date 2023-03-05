39edd40ca197256e86c2c54114e7d37c4736b7af
Anthony Preucil <60394347+apreucil@users.noreply.github.com>
2023-03-05 15:20:20 -0500

    Update WeatherCube.py
    change location to Dumas, TX for testing alerts

--- WeatherCube.py
+++ WeatherCube.py
@@ -52,7 +52,7 @@ pi = pigpio.pi()
 
 #%% Get curernt Location of Device and Locations of Stations
 
-zipcode = "08016"
+zipcode = "79029"
 test_loc = True
 myloc = geocoder.ip('me')
 if myloc.latlng == None or test_loc==True:
