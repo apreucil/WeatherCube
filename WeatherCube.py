314c4f6c9214c1bdb04e0081a42be88364e5f522
Anthony Preucil <60394347+apreucil@users.noreply.github.com>
2023-03-08 18:29:50 -0500

    Update WeatherCube.py
    Moving to La Crosse!

--- WeatherCube.py
+++ WeatherCube.py
@@ -52,7 +52,7 @@ pi = pigpio.pi()
 
 #%% Get curernt Location of Device and Locations of Stations
 
-zipcode = "99747"
+zipcode = "54601"
 test_loc = True
 myloc = geocoder.ip('me')
 if myloc.latlng == None or test_loc==True:
