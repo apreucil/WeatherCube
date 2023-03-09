659983f6ebb3ad10d7491f62cd4afe207bc0dffb
Anthony Preucil <60394347+apreucil@users.noreply.github.com>
2023-03-09 00:01:39 -0500

    Update WeatherCube.py

--- WeatherCube.py
+++ WeatherCube.py
@@ -52,7 +52,7 @@ pi = pigpio.pi()
 
 #%% Get curernt Location of Device and Locations of Stations
 
-zipcode = "54601"
+zipcode = "54601" # what
 test_loc = True
 myloc = geocoder.ip('me')
 if myloc.latlng == None or test_loc==True:
