ea8e5918304054c8a6eca8cb401a1214bc6a5207
Anthony Preucil <anthonypreucil07@gmail.com>
2023-03-09 21:16:32 -0500

    Update WeatherCube.py
    Havana IL for flood

--- WeatherCube.py
+++ WeatherCube.py
@@ -52,7 +52,7 @@ pi = pigpio.pi()
 
 #%% Get curernt Location of Device and Locations of Stations
 
-zipcode = "54601" # what
+zipcode = "62644"
 test_loc = True
 myloc = geocoder.ip('me')
 if myloc.latlng == None or test_loc==True:
